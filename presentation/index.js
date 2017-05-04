4c23c6c240f207a86649c3adb3486eb281c0710d
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2017-05-04 20:20:35 +0800

    Add initial slides for let and const

--- presentation/index.js
+++ presentation/index.js
@@ -3,16 +3,18 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
+  Appear,
+  Code,
   Deck,
   Heading,
+  Image,
   ListItem,
   List,
-  Quote,
   Slide,
   Text
 } from "spectacle";
+
+import CodeSlide from "spectacle-code-slide";
 
 // Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
@@ -24,8 +26,15 @@ import createTheme from "spectacle/lib/themes/default";
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
-// images
-const images = { };
+
+const images = {
+  global: {
+    nuclearExplosion: require("../assets/img/nuclear-explosion.gif")
+  },
+  letConst: {
+    explicitBlockedScopedError: require("../assets/img/let-and-const/block-scoped-let-error.png")
+  }
+};
 
 preloader(images);
 
@@ -45,35 +54,108 @@ export default class Presentation extends React.Component {
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            ES2015 Basics
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+
+          <Text margin="10px 0 0" textColor="tertiary" size={6}>
+            Exequiel Ceasar Navarrete
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+
+        <Slide transition={[]} bgColor="secondary" textColor="primary">
+          <Appear fid="1">
+            <Heading size={1} fit caps lineHeight={1} textColor="primary">
+              What is ES2015?
+            </Heading>
+          </Appear>
+
+          <Appear fid="2">
+            <Text margin="10px 0 0" textColor="primary" size={1}>
+              ES2015 is a significant update to the language, and the first major update to the language since ES5 was standardized in 2009
+            </Text>
+          </Appear>
         </Slide>
+
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={6} textColor="secondary" caps>Features</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <Appear fid="3">
+              <ListItem>let and const</ListItem>
+            </Appear>
+            <Appear fid="4">
+              <ListItem>Enhanced Object literals</ListItem>
+            </Appear>
+            <Appear fid="5">
+              <ListItem>Arrows and Lexical this</ListItem>
+            </Appear>
+            <Appear fid="5">
+              <ListItem>Template Strings</ListItem>
+            </Appear>
+            <Appear fid="6">
+              <ListItem>Classes</ListItem>
+            </Appear>
+            <Appear fid="7">
+              <ListItem>Destructuring</ListItem>
+            </Appear>
+            <Appear fid="8">
+              <ListItem>Default - Rest - Spread</ListItem>
+            </Appear>
+            <Appear fid="9">
+              <ListItem>Promises</ListItem>
+            </Appear>
+            <Appear fid="10">
+              <ListItem>and many more ...</ListItem>
+            </Appear>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+
+        <Slide transition={["zoom"]} bgColor="tertiary">
+          <Text textColor="primary" size={6}>
+            Enough talk! Let's hurry up and go deep dive with the features, shall we?
+          </Text>
+        </Slide>
+
+        <Slide transition={[]} bgColor="secondary" textColor="primary">
+          <Heading size={1} fit lineHeight={1} textColor="primary">
+            let and const
+          </Heading>
+
+          <Appear fid="11">
+            <Text margin="30px 0 0" textColor="primary" size={1}>
+              <Code textColor="primary" size={1}>let</Code> and <Code textColor="primary" size={1}>const</Code>
+              are block-scoped variable declaration constructs.
+            </Text>
+          </Appear>
+
+          <Appear fid="12">
+            <Text margin="30px 0 0" textColor="primary" size={1}>
+              Block scopes include: if, switch, loops, functions and the explicit block scope <Code textColor="primary" size={1}>{ "{}" }</Code>.
+            </Text>
+          </Appear>
+        </Slide>
+
+        <CodeSlide
+          transition={[]}
+          lang="js"
+          code={require("raw-loader!../assets/codes/let-and-const/1.js.example")}
+          ranges={[
+            { loc: [0, 7], title: "let and const" },
+            { loc: [0, 4], title: "Block scope" },
+            { loc: [1, 3], title: "Define variable using let" },
+            { loc: [5, 7], title: "Then we log it" },
+            { loc: [0, 7], title: "What's the output?" }
+          ]} />
+
+        <Slide transition={["slide"]} bgImage={images.global.nuclearExplosion.replace("/", "")} bgDarken={0.75}>
+          <Appear fid="13">
+            <Heading size={1} fit lineHeight={1} textColor="primary">
+              ReferenceError!
+            </Heading>
+          </Appear>
+
+          <Appear fid="14">
+            <Image src={images.letConst.explicitBlockedScopedError.replace("/", "")} margin="30px auto 0" height="300px"/>
+          </Appear>
         </Slide>
       </Deck>
     );
